ffda1783bd6b1fe72c8a90f7236ed07a82f5cfc8
Kunal Sonawane <2kunalsonawane@gmail.com>
2025-04-22 01:11:42 -0700

    No vertical scroll on mobiles for list view #3

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Board from "@/components/board";
 import { useStore } from "./lib/store";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "./components/header";
-import Footer from "./components/footer";
 
 function App() {
   const selectedBoard = useStore((store) => store.selectedBoard);
